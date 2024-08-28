bd04d91c599d693be8b75a49b315cd68b1fc8597
M4rquinhos <marco.37099p@gmail.com>
2024-08-27 18:57:22 -0600

    Viendo eventos

--- PeliculasFrontEnd/src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
+++ PeliculasFrontEnd/src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -8,10 +8,20 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './listado-peliculas.component.html',
   styleUrl: './listado-peliculas.component.css'
 })
-export class ListadoPeliculasComponent  implements OnInit{
-  ngOnInit(): void {
-
-  }
-  @Input({required: true})
+export class ListadoPeliculasComponent {
+  @Input({ required: true })
   peliculas!: any[];
+
+  agregarPelicula() {
+    this.peliculas.push({
+      titulo: "Inception",
+      fechaLanzamiento: new Date('2012-07-03'),
+      precio: 56
+    })
+  }
+
+  remover(pelicula: any) {
+    const indice = this.peliculas.findIndex((peliculaActual: any) => peliculaActual.titulo === pelicula.titulo);
+    this.peliculas.splice(indice, 1);
+  }
 }
