00a3684012c0a89e2c6a785bf78167a8e305ae3c
M4rquinhos <marco.37099p@gmail.com>
2024-08-30 20:09:40 -0600

    Limpieza

--- PeliculasFrontEnd/src/app/app.component.ts
+++ PeliculasFrontEnd/src/app/app.component.ts
@@ -87,9 +87,7 @@ export class AppComponent implements OnInit {
   }
   peliculasEnCines!: any[];
   peliculasProximosEstrenos!: any[];
-  cargando = true;
 
   procesarVoto(voto: number) {
-    alert(`Calificacion otorgada: ${voto}`);
   }
 }
--- PeliculasFrontEnd/src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
+++ PeliculasFrontEnd/src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -14,17 +14,4 @@ import { MatIconModule } from '@angular/material/icon';
 export class ListadoPeliculasComponent {
   @Input({ required: true })
   peliculas!: any[];
-
-  agregarPelicula() {
-    this.peliculas.push({
-      titulo: "Inception",
-      fechaLanzamiento: new Date('2012-07-03'),
-      precio: 56
-    })
-  }
-
-  remover(pelicula: any) {
-    const indice = this.peliculas.findIndex((peliculaActual: any) => peliculaActual.titulo === pelicula.titulo);
-    this.peliculas.splice(indice, 1);
-  }
 }
